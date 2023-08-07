0349c08843b8d698576363ff10a69944bd68778b
Emmanuel Thomas <emmanuel.thomas@northwestern.edu>
2023-08-07 17:27:03 -0500

    check mult open tabs and simulate mouse movement

--- background.js
+++ background.js
@@ -1,20 +1,20 @@
 var intervalId = null;
-
+const KEEP_ACTIVE_INTERVAL = 5 * 60 * 1000; 
 function keepTeamsActive() {
-  chrome.tabs.query({ url: '*://teams.microsoft.com/*' }, function(tabs) {
-    if(tabs.length){
-      chrome.scripting.executeScript({
-        target: {tabId: tabs[0].id},
-        function: () => {
-            document.querySelector('body').click();
-        }
+  chrome.tabs.query({ url: '*://teams.microsoft.com/*', active: true }, function(tabs) {
+    if (tabs.length) {
+      tabs.forEach(tab => {
+        chrome.scripting.executeScript({
+          target: {tabId: tab.id},
+          func: simulateUserActivity,
+          args: []
+        });
       });
-      
+
       if (!intervalId) {
-        intervalId = setInterval(keepTeamsActive, Math.random() * 60000);
+        intervalId = setInterval(keepTeamsActive, KEEP_ACTIVE_INTERVAL);
       }
     } else {
-
       if (intervalId) {
         clearInterval(intervalId);
         intervalId = null;
@@ -23,6 +23,17 @@ function keepTeamsActive() {
   });
 }
 
+function simulateUserActivity() {
+    const body = document.querySelector('body');
+    
+    body.click();
+
+    const mouseEvent = new MouseEvent('mousemove', {
+        clientX: Math.random() * window.innerWidth,
+        clientY: Math.random() * window.innerHeight
+    });
+    body.dispatchEvent(mouseEvent);
+}
 
 keepTeamsActive();
 

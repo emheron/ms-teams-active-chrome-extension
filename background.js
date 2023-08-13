b239c60b718d33f0786132301bee32abd0388210
Emmanuel Thomas <emmanuel.thomas@northwestern.edu>
2023-08-13 11:25:47 -0500

    remove conole logs

--- background.js
+++ background.js
@@ -1,25 +1,22 @@
 var intervalId = null;
-const KEEP_ACTIVE_INTERVAL = 10 * 1000; // 10 seconds
+const KEEP_ACTIVE_INTERVAL = 10 * 1000;
 
 var executedTabs = {};
 
 function keepTeamsActive() {
-  console.log('Starting check for Microsoft Teams tabs...');
 
   chrome.tabs.query({}, function(tabs) {
     const teamsTabs = tabs.filter(tab => tab.url && tab.url.includes('teams.microsoft.com'));
 
     if (teamsTabs.length) {
-      console.log(`Found ${teamsTabs.length} Microsoft Teams tabs. Starting simulation of user activity...`);
       teamsTabs.forEach(tab => {
-        if (!executedTabs[tab.id]) { // Check if this tab has already had the script executed
-          executedTabs[tab.id] = true; // Mark this tab as executed
+        if (!executedTabs[tab.id]) { 
+          executedTabs[tab.id] = true;
           chrome.scripting.executeScript({
             target: { tabId: tab.id },
             files: ['content.js']
           }).then(() => {
-            chrome.tabs.sendMessage(tab.id, { action: 'simulateActivity' }); // Send message to content script
-            console.log(`User activity simulation successful for tab: ${tab.url}`);
+            chrome.tabs.sendMessage(tab.id, { action: 'simulateActivity' });
           }).catch(error => {
             console.error('Error executing script:', error);
           });
@@ -27,18 +24,16 @@ function keepTeamsActive() {
       });
 
       if (!intervalId) {
-        console.log('Starting interval to keep Teams active.');
         intervalId = setInterval(keepTeamsActive, KEEP_ACTIVE_INTERVAL);
       }
     } else {
-      console.log('No Microsoft Teams tabs found.');
       if (intervalId) {
         console.log('Clearing interval.');
         clearInterval(intervalId);
         intervalId = null;
-        executedTabs = {}; // Clear executed tabs if no Teams tabs are found
-      }
+        executedTabs = {}; 
     }
+  }
   });
 }
 
--- content.js
+++ content.js
@@ -3,21 +3,15 @@ if (window.hasRun) {
   }
   
   window.hasRun = true;
-  
-console.log("Content script loaded and running!");
 
 function simulateActivity() {
-  console.log('Starting user activity simulation...');
 
   const body = document.querySelector('body');
-  console.log('Found body element:', body);
 
   // Simulate Click
-  console.log('Simulating click...');
   body.click();
 
   // Simulate Mouse Movement
-  console.log('Simulating mouse movement...');
   const mouseEvent = new MouseEvent('mousemove', {
     clientX: Math.random() * window.innerWidth,
     clientY: Math.random() * window.innerHeight
@@ -25,18 +19,15 @@ function simulateActivity() {
   body.dispatchEvent(mouseEvent);
 
   // Simulate Scrolling
-  console.log('Simulating scrolling...');
   window.scrollBy(0, Math.random() * 100 - 50); // Scrolls up or down by a random amount
 
   // Simulate Keyboard Event
-  console.log('Simulating keyboard event...');
   const keyboardEvent = new KeyboardEvent('keydown', {
     key: ' ',
     code: 'Space',
   });
   body.dispatchEvent(keyboardEvent);
 
-  console.log('Simulation of user activity completed.');
 }
 
 // Listen for a message from the background script to start the simulation

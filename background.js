2fc7d3fc9818f8cce39d5b2e63aa907577458e8b
emheron <m.emmanuel.heron@gmail.com>
2024-02-10 19:06:00 -0600

    enhance logging

--- background.js
+++ background.js
@@ -4,13 +4,19 @@ var executedTabs = {};
 var isActive = true;  
 
 function keepTeamsActive() {
-  if (!isActive) return; 
+  console.log("keepTeamsActive function started.");
+  if (!isActive) {
+    console.log("Extension is not active.");
+    return;
+  } 
 
   chrome.tabs.query({}, function (tabs) {
     const teamsTabs = tabs.filter(tab => tab.url && tab.url.includes('teams.microsoft.com'));
+    console.log("Teams tabs found:", teamsTabs.length);
 
     if (teamsTabs.length) {
       teamsTabs.forEach(tab => {
+        console.log(`Processing tab: ${tab.id}`);
         if (!executedTabs[tab.id]) {
           executedTabs[tab.id] = true;
 
@@ -18,33 +24,42 @@ function keepTeamsActive() {
             target: { tabId: tab.id },
             files: ['content.js']
           }).then(() => {
+            console.log(`Script executed on tab ${tab.id}`);
             chrome.tabs.sendMessage(tab.id, { action: 'simulateActivity' });
           }).catch(error => {
             console.error(`Error executing script on tab ${tab.id}:`, error);
             executedTabs[tab.id] = false;
           });
+        } else {
+          console.log(`Tab ${tab.id} already had script executed.`);
         }
       });
 
       if (!intervalId) {
         intervalId = setInterval(keepTeamsActive, KEEP_ACTIVE_INTERVAL);
+        console.log("Interval set for keepTeamsActive function.");
       }
     } else {
       if (intervalId) {
         clearInterval(intervalId);
         intervalId = null;
         executedTabs = {};
+        console.log("Interval cleared for keepTeamsActive function.");
       }
     }
   });
+  console.log("keepTeamsActive function ended.");
 }
 
 function toggleActiveState() {
+  console.log("toggleActiveState function called.");
   isActive = !isActive;
+  console.log(`Extension active state changed to: ${isActive}`);
   if (!isActive && intervalId) {
     clearInterval(intervalId);
     intervalId = null;
     executedTabs = {};
+    console.log("Extension deactivated and interval cleared.");
   } else if (isActive) {
     keepTeamsActive();
   }
@@ -59,7 +74,3 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   }
   return true;  
 });
-
-
-
-
